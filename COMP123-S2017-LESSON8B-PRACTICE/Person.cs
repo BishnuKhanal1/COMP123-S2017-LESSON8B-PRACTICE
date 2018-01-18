a62bf5c0ea70bbb29eabb1bfaa8f90aa8487ed83
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2018-01-18 17:22:01 -0500

    Initial Commit

---
+++ COMP123-S2017-LESSON8B-PRACTICE/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2017_LESSON8B_PRACTICE
+{
+    /*Note: to add git repo in VS first, go to 'Tools', click 'Extension and Updates', 
+     * click 'Online' and in search box-type 'git'
+     * you will see 'Github' and install it in Vs. 
+     * */
+    class Person
+    {
+    }
+}
--- COMP123-S2017-LESSON8B-PRACTICE/Program.cs
+++ COMP123-S2017-LESSON8B-PRACTICE/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//This is a drver class, a class that drives the rest of the program, codes
+//it is static class, without it, application wouldn't function,
+//it doesn't have to be intantiated, but creates the instance of my program
 namespace COMP123_S2017_LESSON8B_PRACTICE
 {
     class Program
